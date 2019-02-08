6b20389131e03974d0d681ac3d12b0890b86ed96
BissuelQuentin <qbissuel@gmail.com>
2019-02-08 10:04:06 +0100

    Arbre DOM lié au Scheduler

--- InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/GetterInternetPage.java
+++ InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/GetterInternetPage.java
@@ -7,33 +7,68 @@ package com.interoperability.interoperability;
 
 import java.io.BufferedInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author qbiss
  */
-public class GetterInternetPage {
+public class GetterInternetPage extends TimerTask {
+
+    @Override
+    public void run() {
+        HttpURLConnection conn;
+        try {
+            conn = (HttpURLConnection) new URL(
+                    "http://www.office-tourisme-haut-lignon.com/").openConnection();
+            conn.connect();
+            BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+
+            byte[] bytes = new byte[1024];
+            int tmp;
+            FileWriter fichier = new FileWriter("maPage.html");
+            while ((tmp = bis.read(bytes)) != -1) {
+
+                String chaine = new String(bytes, 0, tmp);
+
+                fichier.write(chaine);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(GetterInternetPage.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(GetterInternetPage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    /*
     public static void affPage() throws Exception {
         HttpURLConnection conn = (HttpURLConnection) new URL(
                 "http://www.office-tourisme-haut-lignon.com/").openConnection();
         conn.connect();
- 
+
         BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
- 
+
         byte[] bytes = new byte[1024];
-        int tmp ;
+        int tmp;
         FileWriter fichier = new FileWriter("maPage.html");
-        while( (tmp = bis.read(bytes) ) != -1 ) {
-            
-            String chaine = new String(bytes,0,tmp);
-            
+        while ((tmp = bis.read(bytes)) != -1) {
+
+            String chaine = new String(bytes, 0, tmp);
+
             fichier.write(chaine);
             System.out.print(chaine);
         }
-         
+
         conn.disconnect();
-    }
-    
+    }*/
+
 }
--- InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/Scheduler.java
+++ InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/Scheduler.java
@@ -1,26 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.interoperability.interoperability;
 
-import java.util.Date;
 import java.util.Timer;
 import java.util.concurrent.TimeUnit;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-/**
- *
- * @author Tao
- */
 public class Scheduler {
     public static void main (String[] args) {
-        Tasks t1 = new Tasks();
+        GetterInternetPage getPage = new GetterInternetPage();
         
         Timer t = new Timer();
         
-        t.scheduleAtFixedRate(t1, 0, TimeUnit.MINUTES.toMillis(1));
+        t.scheduleAtFixedRate(getPage, 0, TimeUnit.MINUTES.toMillis(1));
     }
 }
