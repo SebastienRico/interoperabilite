dcd048b76fb1573b7e768d059cdd81b8789fd6d0
SebastienRico <rico.2.sebastien@gmail.com>
2019-03-01 21:35:32 +0100

    Refactoring code

--- InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/objetsDTO/ContactDTO.java
+++ InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/objetsDTO/ContactDTO.java
@@ -1,4 +1,3 @@
-
 package com.interoperability.interoperability.objetsDTO;
 
 import lombok.Data;
@@ -8,31 +7,12 @@ public class ContactDTO extends PersonDTO{
     private String nomContact;
     private String faxContact;
     // max value 2.147.483.647 for an int (in theorie it's ok)
-    private Integer telephoneContact;
+    private int telephoneContact;
     private String emailContact;
     private String siteWebContact;
 
     public ContactDTO() {
     }
     
-    @Override
-    public String toString(){
-        String result = "";
-        if(nomContact != null && !nomContact.isEmpty()){
-            result = nomContact;
-            if(telephoneContact != null && !telephoneContact.toString().isEmpty()){
-                result = result + "\n" + telephoneContact.toString();
-            }
-            if(faxContact != null && !faxContact.isEmpty()){
-                result = result + "\n" + faxContact;
-            }
-            if(emailContact != null && !emailContact.isEmpty()){
-                result = result + "\n" + emailContact;
-            }
-            if(siteWebContact != null && !siteWebContact.isEmpty()){
-                result = result + "\n" + siteWebContact;
-            }
-        }
-        return result;
-    }
+    
 }
