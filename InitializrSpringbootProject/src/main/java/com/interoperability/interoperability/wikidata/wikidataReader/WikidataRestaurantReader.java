96cd00abad84e9076d08727c475909c3da03dd66
Celine <celine.buffat@etu.univ-st-etienne.fr>
2019-03-08 11:59:06 +0100

    Correcting Netbeans errors

--- InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/wikidata/wikidataReader/WikidataRestaurantReader.java
+++ InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/wikidata/wikidataReader/WikidataRestaurantReader.java
@@ -15,7 +15,7 @@ import org.wikidata.wdtk.wikibaseapi.apierrors.MediaWikiApiErrorException;
 public class WikidataRestaurantReader {
     
 
-    public static void readRestaurantPage(RestaurantDTO restaurant){
+    public static void readRestaurantPage(){
         //Example for getting information about an entity, here the example of The Laboratoire Huber Curien, Q900
         //For more examples give a look at: https://github.com/Wikidata/Wikidata-Toolkit-Examples/blob/master/src/examples/FetchOnlineDataExample.java
         /*
