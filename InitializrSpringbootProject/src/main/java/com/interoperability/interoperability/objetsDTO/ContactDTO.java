5aefe60071b52b0900656612b7cbb7733e5dd932
sarahlaurax <sarah.laurax@gmail.com>
2019-03-11 15:19:46 +0100

    Mise en place des contacts lors de la récupération de page HTML

--- InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/objetsDTO/ContactDTO.java
+++ InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/objetsDTO/ContactDTO.java
@@ -6,8 +6,7 @@ import lombok.Data;
 public class ContactDTO extends PersonDTO{
     private String nameContact;
     private String faxContact;
-    // max value 2.147.483.647 for an int (in theorie it's ok)
-    private int phoneContact;
+    private String phoneContact;
     private String mailContact;
     private String websiteContact;
 
