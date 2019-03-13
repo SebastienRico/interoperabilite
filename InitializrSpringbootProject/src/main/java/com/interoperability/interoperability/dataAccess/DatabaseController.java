07f86dbbaa0d249fad068127a809003d2d084261
SebastienRico <rico.2.sebastien@gmail.com>
2019-03-13 23:41:36 +0100

    Updating database

--- InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/dataAccess/DatabaseController.java
+++ InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/dataAccess/DatabaseController.java
@@ -21,6 +21,7 @@ public class DatabaseController {
             connexion = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
             insertItemDocuments();
             insertPropertyDocuments();
+            //insertConnexion();
             Logger.getLogger(DatabaseController.class.getName()).log(Level.INFO, "Inserting data ok");
             connexion.close();
         } catch (SQLException ex) {
@@ -32,8 +33,13 @@ public class DatabaseController {
         Statement statement;
         try {
             statement = connexion.createStatement();
-            statement.execute("insert into item(id, label) values ('50', 'restaurant');");
             // put here all items to insert
+            statement.execute("insert into item(id, label) values ('Q50', 'restaurant');");
+            statement.execute("insert into item(id, label) values ('Q101', 'activite');");
+            statement.execute("insert into item(id, label) values ('Q1298', 'human');");
+            statement.execute("insert into item(id, label) values ('Q99', 'event');");
+            statement.execute("insert into item(id, label) values ('Q88', 'hostel');");
+            statement.execute("insert into item(id, label) values ('Q1640', 'rent');");
             Logger.getLogger(DatabaseController.class.getName()).log(Level.INFO, "Inserting itemDocuments ok");
             statement.close();
         } catch (SQLException ex) {
@@ -46,11 +52,42 @@ public class DatabaseController {
         try {
             statement = connexion.createStatement();
             // put here all properties to insert
-            //statement.execute("insert into property(id, label) values ('50', 'restaurant');");
+            statement.execute("insert into property(id, label) values ('P16', 'instance of');");
+            statement.execute("insert into property(id, label) values ('P1076', 'has for address');");
+            statement.execute("insert into property(id, label) values ('P1064', 'has for capacity');");
+            statement.execute("insert into property(id, label) values ('P61', 'owner');");
+            statement.execute("insert into property(id, label) values ('P1073', 'has for schedule');");
+            statement.execute("insert into property(id, label) values ('P1078', 'has for name');");
+            statement.execute("insert into property(id, label) values ('P1068', 'has for firstname');");
+            statement.execute("insert into property(id, label) values ('P1069', 'has for fax');");
+            statement.execute("insert into property(id, label) values ('P981', 'has for phone');");
+            statement.execute("insert into property(id, label) values ('P1079', 'has for mail');");
+            statement.execute("insert into property(id, label) values ('P172', 'has for website');");
+            statement.execute("insert into property(id, label) values ('P1077', 'has for type');");
+            statement.execute("insert into property(id, label) values ('P1083', 'has for date start');");
+            statement.execute("insert into property(id, label) values ('P1084', 'has for date end');");
+            statement.execute("insert into property(id, label) values ('P1087', 'has for price');");
+            statement.execute("insert into property(id, label) values ('P1088', 'has for stars');");
+            statement.execute("insert into property(id, label) values ('P1089', 'has for periode');");
+            statement.execute("insert into property(id, label) values ('P1085', 'has for disponibility');");
+            statement.execute("insert into property(id, label) values ('P1072', 'has for menu');");
             Logger.getLogger(DatabaseController.class.getName()).log(Level.INFO, "Inserting propertyDocuments ok");
             statement.close();
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseController.class.getName()).log(Level.SEVERE, "Impossible to insert PropertyDocuments", ex);
+        }
+    }
+    
+    private void insertConnexion() throws SQLException {
+        Statement statement;
+        try {
+            statement = connexion.createStatement();
+            // put here all properties to insert
+            statement.execute("insert into connexion(login, password) values ('brigite', 'dusecretariat);");
+            Logger.getLogger(DatabaseController.class.getName()).log(Level.INFO, "Inserting connexions ok");
+            statement.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseController.class.getName()).log(Level.SEVERE, "Impossible to insert connexions", ex);
         }
     }
 }
---
+++ InitializrSpringbootProject/src/main/java/com/interoperability/interoperability/repositories/ConnexionRepository.java
@@ -0,0 +1,15 @@
+package com.interoperability.interoperability.repositories;
+
+import com.interoperability.interoperability.models.ItemDocument;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface ItemDocumentRepository extends CrudRepository<ItemDocument, String> {
+    
+    @Query(value="SELECT DISTINCT * FROM item i WHERE i.id=:id", nativeQuery = true)
+    public ItemDocument findItemDocumentById(@Param("id")Long id);
+    
+    @Query(value="SELECT DISTINCT * FROM item i WHERE i.label=:label", nativeQuery = true)
+    public ItemDocument findItemDocumentByLabel(@Param("label")String label);
+}
